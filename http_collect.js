de1a11e4cc87ac5b5fce5886e17ed57cb3109207
David Castelblanco Benavides <david.castelblanco@hotmail.com>
2015-05-23 15:10:12 -0500

    Ejercicio HTTP Collect resuelto!

---
+++ http_collect.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+var string = '';
+http.get(process.argv[2], function(response){
+    response.on('data',function(data){
+      string += data;
+    });
+
+    response.on('end',function(){
+      console.log(string.length);
+      console.log(string);
+    });
+  
+});
